745121ad211da44cf8cbeb1941ddf5bc50e59f4f
Juan Amiguet Vercher <jamiguet@gmail.com>
2015-02-25 09:30:21 +0100

    Creating empty project.  Changes to be committed: 	new file:   Writeables/pom.xml 	new file:   Writeables/src/main/java/ch/blogspot/prozakcode/writeables/App.java 	new file:   Writeables/src/main/java/ch/blogspot/prozakcode/writeables/App.java~ 	new file:   Writeables/src/test/java/ch/blogspot/prozakcode/writeables/AppTest.java 	new file:   Writeables/target/maven-archiver/pom.properties 	new file:   Writeables/target/surefire-reports/TEST-ch.blogspot.prozakcode.writeables.AppTest.xml 	new file:   Writeables/target/surefire-reports/ch.blogspot.prozakcode.writeables.AppTest.txt

---
+++ Writeables/src/main/java/ch/blogspot/prozakcode/writeables/App.java
@@ -0,0 +1,15 @@
+package ch.blogspot.prozakcode.writeables;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	// I shall not delve more into this as it is in the
+	// tests that all the magic happens
+        System.out.println( "Hello World!" );
+    }
+}
