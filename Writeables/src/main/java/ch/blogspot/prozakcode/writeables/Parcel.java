20be7605e043798c1d80302d1c5db8b9574e212e
Juan Amiguet Vercher <jamiguet@gmail.com>
2015-02-28 18:27:06 +0100

    Adding the readers and writers with a conundrum on creating instances of the contained class.

--- Writeables/src/main/java/ch/blogspot/prozakcode/writeables/Parcel.java
+++ Writeables/src/main/java/ch/blogspot/prozakcode/writeables/Parcel.java
@@ -2,30 +2,34 @@ package ch.blogspot.prozakcode.writeables;
 
 import java.io.DataInput;
 import java.io.DataOutput;
+import java.io.IOException;
+import java.io.EOFException;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedList;
 
-import java.util.Array;
 
 /**
  * Simple object that represents something being transported
+ * It is a parcel hence a form of container.
  */
-public class Parcel implements Writeable{
+public class Parcel<K extends Writeable> implements Writeable{
 
 
     // an array indicating the dimensions od the parcel
     private int[] dimensions;
 
-    // an collection of contents which only have to be writable to be stored.
-    private Collection<Writeable> contents;
+    // an collection of contents which only have to be writable to be stored
+    private Collection<K> contents;
 
     public Parcel(int[] dims){
 	dimensions = Arrays.copyOf(dims,dims.length);
-	contents = new LinkedList<Writeables>();
+	contents = new LinkedList<K>();
     }
 
 
-    public void add(Writable thing){
+    public void add(K thing){
 	synchronized(contents){
 	    contents.add(thing);
 	    contents.notifyAll();
@@ -34,15 +38,37 @@ public class Parcel implements Writeable{
     }
 
     public void readFields(DataInput input){
-	synchronised(contents){
+	synchronized(contents){
+	    try {
+		String[] sDims = input.readUTF().split(",");
+		dimensions = new int[sDims.length];
+		int pos =0;
+		for(String cStr:sDims){
+		    dimensions[pos++] = Integer.parseInt(cStr);
+		}
+
+		contents = new LinkedList<K>();
+		Class<K> factory = Class.forName();
+		while(true){
+		    K cElem = factory.newInstance();
+		    cElem.readFields(input);
+		    contents.add(cElem);
+		}
+		
+	    }catch(EOFException eofe){
+	    }
 
 	    contents.notifyAll();
 	}
     }
 
     public void write(DataOutput output){
-	synchronised(contents){
+	synchronized(contents){
 	    
+	    output.writeUTF(Arrays.toString(dimensions).replace("[","").replace("]",""));
+	    for(Writeable cw: contents){
+		cw.write(output);
+	    }
 	    contents.notifyAll();
 	}
 
@@ -51,7 +77,7 @@ public class Parcel implements Writeable{
     @Override
     public boolean equals(Object other){
 	boolean answer = false;
-	synchronised(contents){
+	synchronized(contents){
 	    if( other instanceof Parcel ){
 		Parcel o = (Parcel) other;
 		answer = Arrays.equals(o.dimensions,this.dimensions) && o.contents.equals(this.contents);
