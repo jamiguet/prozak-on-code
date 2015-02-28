7fa48e592b3984ff55ae602a32c0acbe4a7c4bd1
Juan Amiguet Vercher <jamiguet@gmail.com>
2015-02-28 17:01:18 +0100

    Some more work transferring computer

--- Writeables/src/main/java/ch/blogspot/prozakcode/writeables/Parcel.java
+++ Writeables/src/main/java/ch/blogspot/prozakcode/writeables/Parcel.java
@@ -5,35 +5,60 @@ import java.io.DataOutput;
 
 import java.util.Collection;
 
+import java.util.Array;
+
 /**
  * Simple object that represents something being transported
  */
 public class Parcel implements Writeable{
 
 
+    // an array indicating the dimensions od the parcel
     private int[] dimensions;
-    private String manifest;
+
+    // an collection of contents which only have to be writable to be stored.
     private Collection<Writeable> contents;
 
     public Parcel(int[] dims){
-
+	dimensions = Arrays.copyOf(dims,dims.length);
+	contents = new LinkedList<Writeables>();
     }
 
 
-    public void readFields(DataInput input){
+    public void add(Writable thing){
+	synchronized(contents){
+	    contents.add(thing);
+	    contents.notifyAll();
+	}
+	
+    }
 
+    public void readFields(DataInput input){
+	synchronised(contents){
+
+	    contents.notifyAll();
+	}
     }
 
     public void write(DataOutput output){
+	synchronised(contents){
+	    
+	    contents.notifyAll();
+	}
 
     }
 
     @Override
     public boolean equals(Object other){
-	if( other instanceof Parcel ){
-	    return true;
+	boolean answer = false;
+	synchronised(contents){
+	    if( other instanceof Parcel ){
+		Parcel o = (Parcel) other;
+		answer = Arrays.equals(o.dimensions,this.dimensions) && o.contents.equals(this.contents);
+	    }
+	    contents.notifyAll();
 	}
-	return false;
+	return answer;
     }
 
 }
