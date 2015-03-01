8c66156fb2c529aecbd05868ae66bda46133d428
Juan Amiguet Vercher <jamiguet@gmail.com>
2015-03-01 09:04:48 +0100

    Added afew coments and made sure we are synching on the right mutex.

--- Writeables/src/main/java/ch/blogspot/prozakcode/writeables/Parcel.java
+++ Writeables/src/main/java/ch/blogspot/prozakcode/writeables/Parcel.java
@@ -17,36 +17,67 @@ import java.util.LinkedList;
 public class Parcel<K extends Writeable> implements Writeable{
 
 
-    // an array indicating the dimensions od the parcel
+    // Array indicating the dimensions od the parcel    
     private int[] dimensions;
 
-    Class<K> containee;
+    // Class instance enabling the crafting of containee instances
+    private Class<K> containee;
 
-    // an collection of contents which only have to be writable to be stored
+    // Collection of contents which only have to be writable to be stored
     private Collection<K> contents;
 
-    public Parcel(Class<K> class,int[] dims){
+    /**
+     * Constructor
+     * @param clazz Class<K>  class object for the contents of the parcel
+     * @param dims int[] Phisical dimensions of the parcel
+     */
+    public Parcel(Class<K> clazz,int[] dims){
 	dimensions = Arrays.copyOf(dims,dims.length);
 	contents = new LinkedList<K>();
-	containee = class;
+	containee = clazz;
     }
 
-
+    /**
+     * Method adding an element to the parcel
+     * @param thing represents the thing being added to the parcel
+     */
     public void add(K thing){
-	synchronized(contents){
+	synchronized(this){
 	    contents.add(thing);
 	    contents.notifyAll();
 	}
+    }
+
+    /**
+     * Method checking if the parcel will fit in the specified dimensions
+     * @param containing int array indicating the dimensions of the containing object
+     */
+    public boolean willItFit(int[] containing){
+	boolean res = dimensions.length == containing.length;
+	if(res){
+	    for(int i=0; (i< dimensions.length) && res; i++){
+		res = res && (dimensions[i]<=containing[i]);
+	    }
+	}
+	return res;
 	
     }
 
+    /**
+     * Method returning the content of the pacel
+     * @return A Collection of the contained objects.
+     */
     public Collection<K> contents(){
 	return contents;
     }
 
 
-    public void readFields(DataInput input){
-	synchronized(contents){
+    /**
+     * Method reading the fields of the object from a DataInput object
+     * @param input DataInput object from which the fields are read.
+     */
+    public void readFields(DataInput input) throws IOException{
+	synchronized(this){
 	    try {
 		String[] sDims = input.readUTF().split(",");
 		dimensions = new int[sDims.length];
@@ -56,22 +87,26 @@ public class Parcel<K extends Writeable> implements Writeable{
 		}
 
 		contents = new LinkedList<K>();
-		Class<K> factory = Class.forName();
 		while(true){
 		    K cElem = containee.newInstance();
 		    cElem.readFields(input);
 		    contents.add(cElem);
 		}
 		
-	    }catch(EOFException eofe){
+	    }catch(EOFException eofe){ }
+	    catch(InstantiationException ie){ }
+	    catch(IllegalAccessException iae) { }
+	    finally {
+		contents.notifyAll();
 	    }
-
-	    contents.notifyAll();
 	}
     }
 
-    public void write(DataOutput output){
-	synchronized(contents){
+    /**
+     * Method writing the contents of the object to a DataOutput object
+     */
+    public void write(DataOutput output) throws IOException{
+	synchronized(this){
 	    
 	    output.writeUTF(Arrays.toString(dimensions).replace("[","").replace("]",""));
 	    for(Writeable cw: contents){
@@ -82,10 +117,14 @@ public class Parcel<K extends Writeable> implements Writeable{
 
     }
 
+    /**
+     * Method checking the equality between two parcels
+     * @param other Object against which we check for equality
+     */
     @Override
     public boolean equals(Object other){
 	boolean answer = false;
-	synchronized(contents){
+	synchronized(this){
 	    if( other instanceof Parcel ){
 		Parcel o = (Parcel) other;
 		answer = Arrays.equals(o.dimensions,this.dimensions) && o.contents.equals(this.contents);
